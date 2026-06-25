e96d2e51f800c45c741d19e0365b6de4ef5d9bdf
SnehaF21 <snehafugare4@gmail.com>
2026-06-25 13:28:20 +0530

    Create password_analyzer.py

---
+++ password_analyzer.py
@@ -0,0 +1,27 @@
+password = input("Enter password: ")
+
+score = 0
+
+if len(password) >= 8:
+    score += 1
+
+if any(c.isupper() for c in password):
+    score += 1
+
+if any(c.islower() for c in password):
+    score += 1
+
+if any(c.isdigit() for c in password):
+    score += 1
+
+if any(not c.isalnum() for c in password):
+    score += 1
+
+print("\nPassword Analysis:")
+
+if score <= 2:
+    print("Weak Password")
+elif score <= 4:
+    print("Medium Password")
+else:
+    print("Strong Password")
